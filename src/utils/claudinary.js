83fb7e4a45af11e16cf413a7ffd488be27c21247
Jayesh Dhariyal <jayeshdhariyal@example.com>
2026-09-22 14:58:11 +0530

    Added claudinary and multer setup for uploading images,files,videos

--- src/utils/claudinary.js
+++ src/utils/claudinary.js
@@ -7,21 +7,49 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    //upload the file on cloudinary
-    const response = await cloudinary.uploader.upload(localFilePath, {
+    // upload to Cloudinary
+    const resp = cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // file has been uploaded successfull
-    //console.log("file is uploaded on cloudinary ", response.url);
-    fs.unlinkSync(localFilePath);
-    return response;
+    //
+    console.log("File uploaded to cloudinary Succesfully", resp.url);
+    fs.unlink(localFilePath);
+    return resp;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    fs.unlink(localFilePath);
     return null;
   }
 };
 
 export { uploadOnCloudinary };
+
+// import { v2 as cloudinary } from "cloudinary";
+// import fs from "fs";
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const uploadOnCloudinary = async (localFilePath) => {
+//   try {
+//     if (!localFilePath) return null;
+//     //upload the file on cloudinary
+//     const response = await cloudinary.uploader.upload(localFilePath, {
+//       resource_type: "auto",
+//     });
+//     // file has been uploaded successfull
+//     //console.log("file is uploaded on cloudinary ", response.url);
+//     fs.unlinkSync(localFilePath);
+//     return response;
+//   } catch (error) {
+//     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+//     return null;
+//   }
+// };
+
+// export { uploadOnCloudinary };
