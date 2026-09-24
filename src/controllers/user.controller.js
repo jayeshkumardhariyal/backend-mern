09df0e74394f5e473c31c14dd84e51a66c28ecc7
Jayesh Dhariyal <jayeshdhariyal@example.com>
2026-09-24 11:01:18 +0530

    minor version changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -33,15 +33,24 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is mandatory");
   }
 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+  // console.log(avatar);
+
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-  console.log(avatar);
 
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
